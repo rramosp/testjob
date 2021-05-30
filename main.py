061a89f9588cd91f0ca2a200a68f5c8046b2b641
rlx <rramosp@unal.edu.co>
2021-05-30 15:51:55 -0500

    iteratin

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from time import sleep
 from datetime import datetime
+import os
 
 print ("starting job ...")
 print ("numpy version is", np.__version__)
@@ -10,7 +11,17 @@ import tensorflow as tf
 print('TF VERSION', tf.__version__)
 print ('GPUs', tf.config.list_physical_devices('GPU'))
 
-for i in range(20):
+vars = ['AWS_BATCH_CE_NAME', 'AWS_BATCH_JOB_ID', 'AWS_BATCH_JQ_NAME', 'PATH']
+
+print ("------ ENV VARS -------")
+for var in vars:
+    if var in os.environ.keys():
+       print (f'{var}={os.environ[var]}')
+print ("-----------------------")
+
+print (os.environ)
+
+for i in range(5):
     print (f"{i:3d} {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     sleep(5)
 
