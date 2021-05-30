b3e130fbc3d9b5a149b89dadcd6e81c877330fe0
rlx <rramosp@unal.edu.co>
2021-05-30 15:40:27 -0500

    iteration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from time import sleep
+from datetime import datetime
 
 print ("starting job ...")
 print ("numpy version is", np.__version__)
@@ -7,6 +9,13 @@ print ("numpy version is", np.__version__)
 import tensorflow as tf
 print('TF VERSION', tf.__version__)
 print ('GPUs', tf.config.list_physical_devices('GPU'))
+
+for i in range(20):
+    print (f"{i:3d} {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    sleep(5)
+
+
+
 
 
 
