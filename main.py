be20c2f133b39da9c9c7bff4391a987c67b81777
rlx <rramosp@unal.edu.co>
2021-05-30 15:21:55 -0500

    iteration

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import pandas as pd
 print ("starting job ...")
 print ("numpy version is", np.__version__)
 
+import tensorflow as tf
+print('TF VERSION', tf.__version__)
+print ('GPUs', tf.config.list_physical_devices('GPU'))
+
+
 
