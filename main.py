32bd70a5a83a1247f71c933e0f653ada578f7372
rlx <rramosp@unal.edu.co>
2021-05-31 00:17:58 -0500

    iterate

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 import numpy as np
-import pandas as pd
-from time import sleep
-from datetime import datetime
 from local.lib import utils
-import os, json, boto3
-
-print ("starting job ...")
-print ("numpy version is", np.__version__)
-
+import os
 import tensorflow as tf
-print('TF VERSION', tf.__version__)
+
+print ('TF VERSION', tf.__version__)
 print ('GPUs', tf.config.list_physical_devices('GPU'))
-
-vars = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_DEFAULT_REGION', 'AWS_BATCH_CE_NAME', 'AWS_BATCH_JOB_ID', 'AWS_BATCH_JQ_NAME', 'JOBNAME']
-
-print ("------ ENV VARS -------")
-for var in vars:
-    if var in os.environ.keys():
-       print (f'{var}={os.environ[var]}')
+print ('running job', os.environ['JOBNAME'])
 
 ## get dataset from S3 if required
 utils.command("aws s3 cp s3://clotheme-jobs/data/ACGPN_data.tar.gz .")
 utils.command("tar zxvf ACGPN_data.tar.gz")
-utils.command("ls -las ACGPN_data", printoutput=True)
 
 
 ## simulate some output
